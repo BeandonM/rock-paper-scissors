804d66397eb2caf045cddde7d94d5f66b4ffa146
Brandon Maweu <53877311+BeandonM@users.noreply.github.com>
2024-01-09 21:50:58 -0600

    Added computer choices for rock-paper-scissors

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log("Hello world")
+const choices = ["rock","paper","scissors"]
+function getComputerChoice()
+{
+    let decision = choices[Math.floor(Math.random()*choices.length)];
+    return decision;
+}

c4bea5a5676716399f01703d9c79cde4cc83faa4
Brandon Maweu <53877311+BeandonM@users.noreply.github.com>
2024-01-09 22:08:01 -0600

    Added getting players choice

--- script.js
+++ script.js
@@ -4,3 +4,28 @@ function getComputerChoice()
     let decision = choices[Math.floor(Math.random()*choices.length)];
     return decision;
 }
+
+function getPlayerPrompt()
+{
+    let playerPrompt = ""
+    while (!choices.includes(playerPrompt)){
+        playerPrompt = prompt("Pick between Rock, Paper, Scissors").toLowerCase();
+        if(!choices.includes(playerPrompt)){
+            alert("Please enter a valid choice");
+        }
+    }
+    return playerPrompt;
+}
+function playRound(playerSelection, computerSelection)
+{
+/*
+    let result = ""
+    if(playerSelection == computerSelection)
+    {
+        //Draw
+        result = "Ti"
+    }
+*/
+}
+
+alert(getPlayerPrompt());
